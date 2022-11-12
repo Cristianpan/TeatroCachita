a62a71a09b0218c33017cbac1c17f698ab3af9db
kirake-a <ruben_aalvarado@outlook.com>
2022-11-11 21:45:57 -0600

    Funciones y controlador

---
+++ src/Controlador/ControladorAgregarFunciones.java
@@ -0,0 +1,13 @@
+package Controlador;
+
+import java.awt.event.*;
+
+public class ControladorAgregarFunciones implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        
+        
+    }
+    
+}
---
+++ src/Modelo/Funcion.java
@@ -0,0 +1,83 @@
+package Modelo;
+
+import java.sql.Date;
+import java.sql.Time;
+
+// Falta agregar el campo de duracion en la ventana de agregar funcion
+public class Funcion {
+    private int id;
+    private int obraTeatral;
+    private Date fechaPresentacion;
+    private Time horaPresentacion;
+    private int duracionObraMinutos;
+    private boolean[] AsientoA;
+    private boolean[] AsientoB;
+    private boolean[] AsientoC;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setObraTeatral(int obraTeatral) {
+        this.obraTeatral = obraTeatral;
+    }
+
+    public void setFechaPresentacion(Date fechaPresentacion) {
+        this.fechaPresentacion = fechaPresentacion;
+    }
+
+    public void setHoraPresentacion(Time horaPresentacion) {
+        this.horaPresentacion = horaPresentacion;
+    }
+
+    public void setAsientoA(boolean[] asientoA) {
+        AsientoA = asientoA;
+    }
+
+    public void setAsientoB(boolean[] asientoB) {
+        AsientoB = asientoB;
+    }
+
+    public void setAsientoC(boolean[] asientoC) {
+        AsientoC = asientoC;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getObraTeatral() {
+        return obraTeatral;
+    }
+
+    public Date getFechaPresentacion() {
+        return fechaPresentacion;
+    }
+
+    public Time getHoraPresentacion() {
+        return horaPresentacion;
+    }
+
+    public int getDuracionObraMinutos() {
+        return duracionObraMinutos;
+    }
+
+    public void setDuracionObraMinutos(int duracionObraMinutos) {
+        this.duracionObraMinutos = duracionObraMinutos;
+    }
+
+    public boolean[] getAsientoA() {
+        return AsientoA;
+    }
+
+    public boolean[] getAsientoB() {
+        return AsientoB;
+    }
+
+    public boolean[] getAsientoC() {
+        return AsientoC;
+    }
+
+    
+
+}
