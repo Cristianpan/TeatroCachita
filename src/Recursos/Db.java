be898b3a991fbd9428b44661865808b781d45e53
diana <diana.vazquezro3@gmail.com>
2022-11-29 11:47:23 -0500

    avaces

--- src/Recursos/Db.java
+++ src/Recursos/Db.java
@@ -13,8 +13,7 @@ public class Db {
     public Connection getConexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url="jdbc:mysql://chachitauady.mysql.database.azure.com:3306/chachita?useSSL=true"; 
-            myDbConn=DriverManager.getConnection(url, "CristianPan", "Chachita1");
+            
             
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Asegurese de tener una conexión a la red");
